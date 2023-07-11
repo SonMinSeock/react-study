961b9bb01a5bc0f8b94ecf932acdab59a9d677b1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-11 10:20:06 +0900

    was touched State 처리하기

--- form_pratice/src/components/SimpleInput.js
+++ form_pratice/src/components/SimpleInput.js
@@ -1,9 +1,15 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is Valid!");
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,6 +17,8 @@ const SimpleInput = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    setEnteredNameTouched(true);
+
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
@@ -20,14 +28,16 @@ const SimpleInput = (props) => {
     const enteredValue = nameInputRef.current.value;
     console.log("useRef로 입력 값을 받아오기 : ", enteredValue);
 
-    setEnteredNameIsValid(true);
+    setEnteredName(true);
     setEnteredName("");
     nameInputRef.current.value = "";
   };
 
-  const nameInputClasses = enteredNameIsValid
-    ? "form-control"
-    : "form-control invalid";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmitHandler}>
@@ -39,7 +49,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           ref={nameInputRef}
         />
-        {!enteredNameIsValid ? (
+        {nameInputIsInvalid ? (
           <p className="error-text">Name must not be empty.</p>
         ) : null}
       </div>
