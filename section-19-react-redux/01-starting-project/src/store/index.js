7886c7deab7db1bc5d727d99fb5a86721f5eba4c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-17 15:24:57 +0900

    리덕스 툴킷 State 연결하기

--- section-19-react-redux/01-starting-project/src/store/index.js
+++ section-19-react-redux/01-starting-project/src/store/index.js
@@ -1,9 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
-const { createStore } = require("redux");
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+// const { createStore } = require("redux");
 
 const initailState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState: initailState,
   reducers: {
@@ -22,20 +22,22 @@ createSlice({
   },
 });
 
-function counterReducer(state = initailState, action) {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  } else if (action.type === "increase") {
-    return { counter: state.counter + action.amount, showCounter: state.showCounter };
-  } else if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  } else if (action.type === "toggle") {
-    return { counter: state.counter, showCounter: !state.showCounter };
-  }
+// function counterReducer(state = initailState, action) {
+//   if (action.type === "increment") {
+//     return { counter: state.counter + 1, showCounter: state.showCounter };
+//   } else if (action.type === "increase") {
+//     return { counter: state.counter + action.amount, showCounter: state.showCounter };
+//   } else if (action.type === "decrement") {
+//     return { counter: state.counter - 1, showCounter: state.showCounter };
+//   } else if (action.type === "toggle") {
+//     return { counter: state.counter, showCounter: !state.showCounter };
+//   }
 
-  return state;
-}
+//   return state;
+// }
 
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
