e48439a23516e99d485f8176068b33d15b6cc3ba
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-29 19:31:45 +0900

    레이아웃 및 중첩된 라우트

--- section21-router/01-starting-project/src/App.js
+++ section21-router/01-starting-project/src/App.js
@@ -1,6 +1,8 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import MainNavigation from "./components/MainNavigation";
+import RootLayout from "./pages/Root";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -12,8 +14,14 @@ import Products from "./pages/Products";
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Products /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
 ]);
 
 function App() {
