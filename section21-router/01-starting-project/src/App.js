281d578496c44c483033610d54fa399e74ef0d33
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-29 18:51:51 +0900

    라우트를 정의하는 다른 방법

--- section21-router/01-starting-project/src/App.js
+++ section21-router/01-starting-project/src/App.js
@@ -1,16 +1,19 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
 ]);
 
 function App() {
