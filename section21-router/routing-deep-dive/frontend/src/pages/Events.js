059b78cd5cb7bd22882c97efa5b1dba7c3cdefab
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-02-19 18:11:18 +0900

    loader() 데이터를 라우트 컴포넌트에서 사용하기

--- section21-router/routing-deep-dive/frontend/src/pages/Events.js
+++ section21-router/routing-deep-dive/frontend/src/pages/Events.js
@@ -1,28 +1,11 @@
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EvnetsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
-
+  const events = useLoaderData();
   return (
     <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-        {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-      </div>
+      <EventsList events={events} />
     </>
   );
 }
