dfb531f7904e0144526e64cc48d384e6e2ce9ceb
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-02-23 17:40:42 +0900

    오류 데이터를 추출하고 응답 내보내기

--- section21-router/routing-deep-dive/frontend/src/pages/Error.js
+++ section21-router/routing-deep-dive/frontend/src/pages/Error.js
@@ -1,7 +1,27 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 function Error() {
+  const error = useRouteError();
+
+  let title = "An Error occured!";
+  let message = "Something wend wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found!";
+    message = "Could not found resource page!";
+  }
   return (
     <>
-      <h1>An Error occured!</h1>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </>
   );
 }
