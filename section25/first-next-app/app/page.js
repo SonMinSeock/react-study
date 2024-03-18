3a2bc8a97845b6bcd9604bf44f573953c72dbfb1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-18 09:44:14 +0900

    파일 기반 라우팅과 리액트 서버 컴포넌트의 이해

---
+++ section25/first-next-app/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  console.log("Home 컴포넌트 서버사이드에서 실행중...");
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
