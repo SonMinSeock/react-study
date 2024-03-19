e19736f4f9d10947066787df09b68bc271e0e1ce
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-19 16:24:29 +0900

    페이지 간 이동—옳고 그른 해결책

--- section25/first-next-app/app/page.js
+++ section25/first-next-app/app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log("Home 컴포넌트 서버사이드에서 실행중...");
   return (
@@ -5,6 +7,9 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
