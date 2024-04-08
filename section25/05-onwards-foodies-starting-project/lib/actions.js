db27b408b50592a7cf75d8adbb182c3731f29eba
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-09 07:33:01 +0900

    서버 사이드 입력 유효성 검사하는 방법

--- section25/05-onwards-foodies-starting-project/lib/actions.js
+++ section25/05-onwards-foodies-starting-project/lib/actions.js
@@ -3,6 +3,10 @@
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
@@ -12,6 +16,20 @@ export async function shareMeal(formData) {
     instructions: formData.get("instructions"),
     image: formData.get("image"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid Input");
+  }
+
   await saveMeal(meal);
   redirect("/meals");
 }
